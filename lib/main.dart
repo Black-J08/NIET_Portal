befcb62b583783580c0677f1c22a39c4ca3fe36b
Nitin Anand <nitin88mehta@gmail.com>
2020-11-26 02:45:04 +0530

    Built Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'NIET Portal',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue[700],
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -22,9 +22,108 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('NIET Portal'),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Text(
-          'Hello World',
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.width / 4,
+                      margin: EdgeInsets.all(5),
+                      child: RaisedButton(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.description_outlined,
+                              size: 52,
+                            ),
+                            Text('Notices'),
+                          ],
+                        ),
+                        color: Colors.blue[700],
+                        textColor: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.width / 4,
+                      margin: EdgeInsets.all(5),
+                      child: RaisedButton(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.email_outlined,
+                              size: 52,
+                            ),
+                            Text('Mail'),
+                          ],
+                        ),
+                        color: Colors.blue[700],
+                        textColor: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.width / 4,
+                      margin: EdgeInsets.all(5),
+                      child: RaisedButton(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.email_outlined,
+                              size: 52,
+                            ),
+                            Text('Exam Results'),
+                          ],
+                        ),
+                        color: Colors.blue[700],
+                        textColor: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.width / 4,
+                      margin: EdgeInsets.all(5),
+                      child: RaisedButton(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.account_circle_outlined,
+                              size: 52,
+                            ),
+                            Text('My Profile'),
+                          ],
+                        ),
+                        color: Colors.blue[700],
+                        textColor: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );

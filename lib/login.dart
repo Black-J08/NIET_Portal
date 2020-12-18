abd8db8d93b613139971d2f7ef6aa09f8a4d6036
avlearner <iamaryanve18@gmail.com>
2020-12-18 20:40:10 +0530

    login and ui modified

--- lib/login.dart
+++ lib/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'main.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -35,46 +36,70 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text(
+          'Login',
+          style: TextStyle(
+              fontWeight: FontWeight.w500, fontSize: 26, color: Colors.white),
+        ),
         centerTitle: true,
+        toolbarHeight: 47,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
+              color: Colors.blue[200],
+              padding: EdgeInsets.all(2),
               margin: EdgeInsets.fromLTRB(50, 10, 50, 7),
               child: TextField(
                 controller: user,
                 autofocus: true,
-                decoration: InputDecoration(hintText: 'Enter User ID'),
+                decoration: InputDecoration(
+                  hintText: 'Enter User ID',
+                  hintStyle: TextStyle(fontSize: 18, color: Colors.white),
+                ),
               ),
             ),
             Container(
+              color: Colors.blue[200],
+              padding: EdgeInsets.all(2),
               margin: EdgeInsets.fromLTRB(50, 7, 50, 10),
               child: TextField(
                 controller: pass,
-                decoration: InputDecoration(hintText: 'Enter Password'),
+                decoration: InputDecoration(
+                  hintText: 'Enter Password',
+                  hintStyle: TextStyle(fontSize: 18, color: Colors.white),
+                ),
                 obscureText: true,
               ),
             ),
-            Container(
-              child: RaisedButton(
-                child: Text('Login'),
-                onPressed: () async {
-                  await headlessLoginWebView.webViewController.evaluateJavascript(source: """
+            SizedBox(height: 80),
+            RaisedButton(
+              color: Colors.blue[800],
+              padding: EdgeInsets.all(8),
+              child: Text(
+                'Login',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+              onPressed: () async {
+                await headlessLoginWebView.webViewController
+                    .evaluateJavascript(source: """
                   document.getElementById('txtUserName').value = '${user.text}';
                   document.getElementById('txtPassword').value = '${pass.text}';
                   document.getElementById('Ulogin').click();
                   """);
 
-                  final dir = await getApplicationDocumentsDirectory();
-                  final file = File('${dir.path}/data.txt');
-                  file.writeAsString('${user.text}:${pass.text}');
-
-                  Navigator.pop(context);
-                },
-              ),
+                final dir = await getApplicationDocumentsDirectory();
+                final file = File('${dir.path}/data.txt');
+                file.writeAsString('${user.text}:${pass.text}');
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ),
+                );
+                //  Navigator.pop(context);
+              },
             ),
           ],
         ),
